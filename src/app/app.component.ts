5ede160a5848c16e1e98e5d5f398898ed6b21d9b
jorg3lui5 <jorg3lui5@hotmail.com>
2021-03-15 11:29:37 -0500

    Ion-menu-parte2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Componente } from './interfaces/interfaces';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  componentes: Observable<Componente[]>;
+
+  constructor(private _dataService: DataService) 
+  {
+
+  }
+
+  ngOnInit() {
+    this.componentes=this._dataService.getMenuOpts();
+  }
 }
