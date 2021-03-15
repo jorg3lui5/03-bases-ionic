001b5f97900a197990a530b8424f082b9e924992
jorg3lui5 <jorg3lui5@hotmail.com>
2021-03-15 14:26:26 -0500

    Ion-modal-parte1

---
+++ src/app/pages/modal/modal.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalInfoPage } from '../modal-info/modal-info.page';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.page.html',
+  styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit {
+
+  constructor(public modalController: ModalController
+    
+  ) {
+
+  }
+
+  ngOnInit() {
+  }
+
+  async mostrarModal(){
+    const modal = await this.modalController.create({
+      component: ModalInfoPage
+    });
+    await modal.present();
+  }
+
+}
